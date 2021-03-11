937249468032c2be2cd8cc169bcb0a8d077b2209
Ariana Winters <arianaw15@gmail.com>
2021-03-10 17:21:35 -0700

    first iteration of adding a new employee

--- employee.js
+++ employee.js
@@ -11,90 +11,84 @@ const connection = mysql.createConnection({
     // Be sure to update with your own MySQL password!
     password: 'Coggy0501!',
     database: 'employees_tracking_db',
-  });
+});
 
-  connection.connect((err) => {
+connection.connect((err) => {
     if (err) {
-      console.error(`error connecting: ${err.stack}`);
-      return;
+        console.error(`error connecting: ${err.stack}`);
+        return;
     }
     console.log(`connected as id ${connection.threadId}`);
-  });
+});
 
-const mainMenuTable = ()=>
-inquirer
-.prompt([{
-    type: 'list',
-    message: 'What would you like to do?',
-    name: 'menuList',
-    choices: ['Add an employee', 'Add a deparment', 'Add a role', 'View departments', 'View roles', 'View employees', 'Update employee roles']
-}, ])
-
-const buildEmployee = ()=>
-inquirer.prompt([{
-        type: 'input',
-        message: 'First name:',
-        name: 'firstName',
-    },
-    {
-        type: 'input',
-        message: 'Last name:',
-        name: 'lastName',
-    },
-    {
+const mainMenuTable = () =>
+    inquirer
+    .prompt([{
         type: 'list',
-        message: 'Department:',
-        name: 'department',
-        choices: ['Account Management',]
-    },
-    {
-        type: 'list',
-        message: 'Manager:',
-        name: 'manager',
-        choices: ['Ariana Winters',]
-    },
+        message: 'What would you like to do?',
+        name: 'menuList',
+        choices: ['Add an employee', 'Add a deparment', 'Add a role', 'View departments', 'View roles', 'View employees', 'Update employee roles']
+    }, ])
 
-])
+const buildEmployee = () =>
+    inquirer.prompt([{
+            type: 'input',
+            message: 'First name:',
+            name: 'firstName',
+        },
+        {
+            type: 'input',
+            message: 'Last name:',
+            name: 'lastName',
+        },
+        {
+            type: 'list',
+            message: 'Role ID:',
+            name: 'roleId',
+            choices: [1, ]
+        },
+        {
+            type: 'list',
+            message: 'Manager ID:',
+            name: 'managerId',
+            choices: [1, ]
+        },
+
+    ])
 
 
 mainMenuTable()
-.then((response)=> {
-    console.log(response.menuList);
-    if((response.menuList) === 'Add an employee'){
-buildEmployee()
-    };
-    if((response.menuList) === 'View departments'){
-            const sqlCommand = 'SELECT * FROM department ORDER BY id ASC';
-            connection.query(sqlCommand,(err,result)=>{
-                if (err) throw err;
-                console.table(result);
-        });
-    };
-    if((response.menuList) === 'View roles'){
-        const sqlCommand = 'SELECT * FROM role ORDER BY id ASC';
-    connection.query(sqlCommand,(err,result)=>{
-        if (err) throw err;
-        console.table(result);
-    });
-};
-if((response.menuList) === 'View employees'){
-    const sqlCommand = 'SELECT * FROM employee ORDER BY id ASC';
-      connection.query(sqlCommand,(err,result)=>{
-          if (err) throw err;
-          console.table(result);
-});
-};
-})
-.catch((err) => console.error(err))
-
-
-
-// employeeTableBuild()
-// .then((response) => console.log(JSON.stringify(response.menuList)))
-// .then((response) => {
-//     if(JSON.stringify(response) === 'Add an employee'){
-//         buildEmployee()
-//         .then((response) => console.log(response))
-//     };
-// })
-// .catch((err) => console.error(err))
+    .then((response) => {
+            console.log(response.menuList);
+            if ((response.menuList) === 'View departments') {
+                const sqlCommand = 'SELECT * FROM department ORDER BY id ASC';
+                connection.query(sqlCommand, (err, result) => {
+                    if (err) throw err;
+                    console.table(result);
+                });
+            };
+            if ((response.menuList) === 'View roles') {
+                const sqlCommand = 'SELECT * FROM role ORDER BY id ASC';
+                connection.query(sqlCommand, (err, result) => {
+                    if (err) throw err;
+                    console.table(result);
+                });
+            };
+            if ((response.menuList) === 'View employees') {
+                const sqlCommand = 'SELECT * FROM employee ORDER BY id ASC';
+                connection.query(sqlCommand, (err, result) => {
+                    if (err) throw err;
+                    console.table(result);
+                });
+            };
+                if ((response.menuList) === 'Add an employee') {
+                    buildEmployee()
+                        .then((response) => {
+                                const sqlCommand = `INSERT INTO employee (first_name,last_name, role_id,manager_id) VALUES (${response.firstName},${response.lastName},${response.roleId},${response.managerId})`;
+                                connection.query(sqlCommand, (err, result) => {
+                                    if (err) throw err;
+                                });
+                            });
+                        };
+                })
+            .catch((err) => console.error(err))
